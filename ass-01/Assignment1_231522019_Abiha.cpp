0b36d6f4b2b19d7e7f683f8af7262d1922498a3b
SyedaAbiha <128927568+SyedaAbiha@users.noreply.github.com>
2023-04-02 22:36:15 +0500

    Update Assignment1_231522019_Abiha.cpp

--- ass-01/Assignment1_231522019_Abiha.cpp
+++ ass-01/Assignment1_231522019_Abiha.cpp
@@ -18,3 +18,30 @@ int board[fixed_size][fixed_size]; // Initializing the board with the specified 
 
 int score = 0;                     // Score variable which will be used throughout to store points per game
 int highScore = 0;                 // High score to save the highest score throughout the played games in one instance
+
+// Function to initialize the main board (the starting)
+void initBoard() { 
+    // Initialize all elements to 0
+    for (int i = 0; i < fixed_size; i++) {     // for i in range(fixed_size) [ROWS]
+        for (int j = 0; j < fixed_size; j++) { // for j in range(fixed_size) [COLUMNS]
+            board[i][j] = 0;                   // Setting all elements to value '0'
+        }
+    }
+    // Adding two random tiles
+    srand(time(0));
+    // Tile-1 'x' and 'y'
+    int x1 = rand() % fixed_size; 
+    int y1 = rand() % fixed_size;
+    // Tile-2 'x' and 'y'
+    int x2 = rand() % fixed_size;
+    int y2 = rand() % fixed_size;
+
+    while (x1 == x2 && y1 == y2) { // Ensure that the two random tiles are not the same
+        // If they are same, change the value of the other
+        x2 = rand() % fixed_size;
+        y2 = rand() % fixed_size;
+    }
+    // Setting random values (2 or 4) in those selected random tiles
+    board[x1][y1] = (rand() % 2 + 1) * 2; 
+    board[x2][y2] = (rand() % 2 + 1) * 2;
+}
